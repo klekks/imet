0ef922fee2786d15262f7e4eb2e8de5465b1d32e
Илья Петров <petrov.ip@edu.spbstu.ru>
2022-03-28 00:09:05 +0300

    bot2

--- bot.py
+++ bot.py
@@ -35,55 +35,53 @@ def clear_ruz(message):
 def ruz_group_wait(message, events):
     clear_ruz(message)
     group = message.text
-    print(1)
+
     from requests import get
     import re
     res = get("https://ruz.spbstu.ru/search/groups?q={0}".format(group))
-    print(2)
+
     if res.status_code != 200:
         return
-    print(3)
+
     data = re.search(r"<script>\s*window\.__INITIAL_STATE__ = (.*)\s*;\s*</script>", res.text).group(1)
-    print(4)
+
     data = loads(data)
     group_id = data["searchGroup"]["data"][0]["id"]
     group_name = data["searchGroup"]["data"][0]["name"]
-    print(5)
+
     message.from_user.user_group = group_name
     message.from_user.save()
-    print(6)
+
     res = get("https://ruz.spbstu.ru/api/v1/ruz/scheduler/{0}".format(group_id))
-    print(7)
+
     if res.status_code != 200:
         return
-    print(8)
+
     ruz = loads(res.text)
     today = datetime.datetime.today().strftime("%Y-%m-%d")
     week = list(ruz['days'])
-    print(list(filter(lambda day: day['date'] == today, week)))
-    ruz_for_today = list(filter(lambda day: day['date'] == today, week))[0]
-    print(10)
-    lessons = ruz_for_today['lessons']
 
+    ruz_for_today = list(filter(lambda day: day['date'] == today, week))
     ruz = ["Расписание для {1} на {0}:".format(today, group_name), ]
-
-    for i in lessons:
-        ruz.append(
-         "📍 {0}: {1} ({2}, ауд. {3})".format(i['time_start'], i['subject_short'], i['auditories'][0]['building']['name'], i['auditories'][0]['name'])
-        )
-    if len(ruz) == 1:
+    if len(ruz_for_today) == 0:
         ruz.append("На сегодня пар нет.")
-    print(11)
+    else:
+        ruz_for_today = ruz_for_today[0]
+        lessons = ruz_for_today['lessons']
+
+        for i in lessons:
+            ruz.append(
+             "📍 {0}: {1} ({2}, ауд. {3})".format(i['time_start'], i['subject_short'], i['auditories'][0]['building']['name'], i['auditories'][0]['name'])
+            )
+
     message.from_user.current_page = Page.select(Page).where(Page.id == 1).get()
     message.from_user.save()
     reply_markup = gen_markup(message.from_user.current_page.id)
-    print(12)
     for i in events:
         i.delete_instance()
 
     for msg in ruz:
         bot.send_message(message.chat.id, msg, reply_markup=reply_markup)
-    print(13)
 
 ACTIONS = {
     "sayhi": sayhi,
