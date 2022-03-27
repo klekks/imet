cf887833bc4b26b6f62c928bb46c46df104862ed
Илья Петров <petrov.ip@edu.spbstu.ru>
2022-03-27 23:34:07 +0300

    bot2

--- build.sh
+++ build.sh
@@ -3,13 +3,9 @@
 echo "Start build.sh"
 
 pkill python3
-pkill flask
 
 pip3 install -r ./requirements.txt&
 python3 ./bot.py&
 
-# FLASK_APP=./backend.py
-#export FLASK_ENV=development
-#flask run&
 
 echo "End build.sh !!!!!"
