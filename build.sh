551a171092caa1491fc861515d74ea4c84c5e247
Илья Петров <petrov.ip@edu.spbstu.ru>
2022-03-27 23:40:53 +0300

    bot2

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 
 echo "Start build.sh"
 
-sudo pkill python3
+sudo pkill python3 > pkill.log
 
-pip3 install -r ./requirements.txt
+pip3 install -r ./requirements.txt > pip.log
 
-python3 bot.py&
+python3 bot.py& > python.log
 
 
 echo "End build.sh !!!!!"
